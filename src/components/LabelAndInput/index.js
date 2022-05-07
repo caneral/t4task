eacf344cff3a3223477fc3edb38e32108887c2ee
Caner Al <caneral5858@gmail.com>
2022-05-07 12:19:20 +0300

    React-hook-form added.

--- src/components/LabelAndInput/index.js
+++ src/components/LabelAndInput/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-const LabelAndInput = ({ label, type }) => {
+const LabelAndInput = ({ label, type, hookform }) => {
   return (
     <div className="sm:w-80 flex flex-col gap-1">
-      <label className="font-medium text-sm text-gray-500">
-        {label}
-      </label>
+      <label className="font-medium text-sm text-gray-500">{label}</label>
       <input
+        {...hookform}
         className="bg-white rounded-md p-2 border-[1px] border-gray-300 focus:border-[#7367f0] focus:outline-none text-sm text-gray-500"
         type={type}
       />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,19 +3,51 @@ import Card from "../components/Card/Card";
 import LabelAndInput from "../components/LabelAndInput";
 import logo from "../assets/images/logo.png";
 import Button from "../components/Button";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="min-h-screen bg-[#f8f8f8] flex items-center justify-center">
       <Card className="flex flex-col gap-4 ">
         <img src={logo} />
         <div className="flex flex-col gap-2 my-4">
-        <h3 className="font-medium text-gray-500">Welcome to T4 Analytics!</h3>
-        <p className="font-normal sm:w-80 text-gray-400 text-sm">Please login to your account and jumpstart your digital transformation with breakthrough impact</p>
+          <h3 className="font-medium text-gray-500">
+            Welcome to T4 Analytics!
+          </h3>
+          <p className="font-normal sm:w-80 text-gray-400 text-sm">
+            Please login to your account and jumpstart your digital
+            transformation with breakthrough impact
+          </p>
         </div>
-        <LabelAndInput label="Username" type="text" />
-        <LabelAndInput label="Password" type="password" />
-        <Button title="Login" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <LabelAndInput
+            label="Username"
+            type="text"
+            hookform={{ ...register("username", { required: true }) }}
+          />
+          {errors.username && (
+            <span className="text-red-600 text-sm">This field is required</span>
+          )}
+          <LabelAndInput
+            label="Password"
+            type="password"
+            hookform={{ ...register("password", { required: true }) }}
+          />
+          {errors.password && (
+            <span className="text-red-600 text-sm">This field is required</span>
+          )}
+          <Button title="Login" />
+        </form>
       </Card>
     </div>
   );
