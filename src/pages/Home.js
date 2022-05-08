05b6fcfec75b5c38100214a31f5f3802fc95c203
Caner Al <caneral5858@gmail.com>
2022-05-08 13:21:05 +0300

    Url parse

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,24 @@
-import React,{useEffect, useState} from 'react'
-import t4Api from '../configs/t4Api'
-
+import React, { useEffect, useState } from "react";
+import t4Api from "../configs/t4Api";
+import queryString from "query-string";
 
 const Home = () => {
-    useEffect(() => {
-        t4Api.serverAuth({server_id:1, datasource_id:4}).then((response) => {
-            console.log(response)
-        })
-    },[])
+  const [ticket, setTicket] = useState();
+
+  useEffect(() => {
+    t4Api.serverAuth({ server_id: 1, datasource_id: 4 }).then((response) => {
+      const qlikTicket = queryString.parseUrl(response.data.url).query
+        .qlikTicket;
+      localStorage.setItem("qlikTicket", qlikTicket);
+      setTicket(qlikTicket);
+    });
+  }, []);
 
   return (
-    <div>Home</div>
-  )
-}
+    <div className="min-h-screen flex justify-center items-center text-xl font-bold">
+      qlikTicket: {ticket}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
